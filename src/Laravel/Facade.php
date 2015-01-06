3212d4d8c59675fd72ce88c8fb8aa70d0182d03a
ARCANEDEV <contact@arcanedev.net>
2015-01-06 18:37:38 +0000

    Adding Laravel Support

---
+++ src/Laravel/Facade.php
@@ -0,0 +1,11 @@
+<?php namespace Arcanedev\Head\Laravel;
+
+class Facade extends \Illuminate\Support\Facades\Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor() { return 'arcanedev.head'; }
+}
---
+++ src/Laravel/ServiceProvider.php
@@ -0,0 +1,71 @@
+<?php namespace Arcanedev\Head\Laravel;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Properties
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Package Name
+     *
+     * @var string
+     */
+    protected $package = 'arcanedev/lara-head';
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package($this->package, null, __DIR__);
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerServices();
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [
+            'arcanedev.head'
+        ];
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Package Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    private function registerServices()
+    {
+        $this->app->singleton('arcanedev.head', function($app) {
+            $configs = $app['config']->get('lara-head::config');
+
+            return new Head($configs);
+        });
+    }
+}

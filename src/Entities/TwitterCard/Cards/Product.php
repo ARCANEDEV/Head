11f24d8d7d332915338b6e810b9ffdc7128caa56
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-07-18 01:14:41 +0000

    Complete the Product implementation

--- src/Entities/TwitterCard/Cards/Product.php
+++ src/Entities/TwitterCard/Cards/Product.php
@@ -1,5 +1,7 @@
 <?php namespace Arcanedev\Head\Entities\TwitterCard\Cards;
 
+use Arcanedev\Head\Entities\TwitterCard\TwitterMetaBuilder as Builder;
+
 /**
  * Class Product
  * @package Arcanedev\Head\Entities\TwitterCard\Cards
@@ -12,8 +14,251 @@ class Product extends AbstractCard
      |  Properties
      | ------------------------------------------------------------------------------------------------
      */
-    /** @var string */
+    /**
+     * Twitter card type
+     *
+     * @var string
+     */
     protected $card = 'product';
+
+    /**
+     * The Twitter @username the card should be attributed to.
+     *
+     * @var string
+     */
+    protected $site        = '';
+
+    /**
+     * @username of content creator
+     *
+     * @var string
+     */
+    protected $creator = '';
+
+    /**
+     * Title should be concise and will be truncated at 70 characters.
+     *
+     * @var string
+     */
+    protected $title       = '';
+
+    /**
+     * A description that concisely summarizes the content of the page,
+     * as appropriate for presentation within a Tweet.
+     * Do not re-use the title text as the description, or use this field
+     * to describe the general services provided by the website.
+     * Description text will be truncated at the word to 200 characters.
+     *
+     * @var string
+     */
+    protected $description = '';
+
+    /**
+     * URL to a unique image representing the content of the page.
+     * Do not use a generic image such as your website logo, author photo, or other image that
+     * spans multiple pages.
+     * The image must be a minimum size of 120px by 120px and must be less than 1MB in file size.
+     * For an expanded tweet and its detail page, the image will be cropped to a 4:3 aspect ratio
+     * and resized to be displayed at 120px by 90px. The image will also be cropped and resized
+     * to 120px by 120px for use in embedded tweets.
+     *
+     * @var string
+     */
+    protected $image = '';
+
+    /**
+     * This field expects a string, and you can specify values for labels such as
+     * price, items in stock, sizes, etc
+     * @var string
+     */
+    protected $data1 = '';
+
+    /**
+     * This field also expects a string, and allows you to specify the types of
+     * data you want to offer (price, country, etc.)
+     *
+     * @var string
+     */
+    protected $label1 = '';
+
+    /**
+     * This field expects a string, and you can specify values for labels such as price,
+     * items in stock, sizes, etc
+     *
+     * @var string
+     */
+    protected $data2 = '';
+
+    /**
+     * This field also expects a string, and allows you to specify the types of data you
+     * want to offer (price, country, etc.)
+     *
+     * @var string
+     */
+    protected $label2 = '';
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Getters & Setters
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Get creator username
+     *
+     * @return string
+     */
+    public function getCreator()
+    {
+        return $this->creator;
+    }
+
+    /**
+     * Set creator username
+     *
+     * @param  string $creator
+     *
+     * @return self
+     */
+    public function setCreator($creator)
+    {
+        $this->checkCreator($creator);
+        $this->creator = $creator;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set description
+     *
+     * @param  string $description
+     *
+     * @return self
+     */
+    public function setDescription($description)
+    {
+        $this->checkDescription($description);
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get image
+     *
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Set image
+     *
+     * @param  string $image
+     *
+     * @return self
+     */
+    public function setImage($image)
+    {
+        $this->checkImage($image);
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getData1()
+    {
+        return $this->data1;
+    }
+
+    /**
+     * @param  string $data1
+     *
+     * @return self
+     */
+    public function setData1($data1)
+    {
+        $this->checkNotEmpty($data1, 'The product\'s data1 is required.');
+        $this->data1 = $data1;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLabel1()
+    {
+        return $this->label1;
+    }
+
+    /**
+     * @param  string $label1
+     *
+     * @return self
+     */
+    public function setLabel1($label1)
+    {
+        $this->checkNotEmpty($label1, 'The product\'s label1 is required.');
+        $this->label1 = $label1;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getData2()
+    {
+        return $this->data2;
+    }
+
+    /**
+     * @param  string $data2
+     *
+     * @return self
+     */
+    public function setData2($data2)
+    {
+        $this->checkNotEmpty($data2, 'The product\'s data2 is required.');
+        $this->data2 = $data2;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLabel2()
+    {
+        return $this->label2;
+    }
+
+    /**
+     * @param  string $label2
+     *
+     * @return self
+     */
+    public function setLabel2($label2)
+    {
+        $this->checkNotEmpty($label2, 'The product\'s label2 is required.');
+        $this->label2 = $label2;
+
+        return $this;
+    }
 
     /* ------------------------------------------------------------------------------------------------
      |  Main Functions
@@ -30,6 +275,78 @@ class Product extends AbstractCard
             return '';
         }
 
-        return '';
+        $attributes = array_intersect_key(get_object_vars($this), array_flip([
+            'card',
+            'site',
+            'creator',
+            'title',
+            'description',
+            'image',
+            'data1',
+            'label1',
+            'data2',
+            'label2',
+        ]));
+
+        return Builder::html($attributes);
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Check Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Check if empty
+     *
+     * @return bool
+     */
+    public function isEmpty()
+    {
+        return parent::isEmpty() || empty($this->title) || empty($this->description) || empty($this->image);
+    }
+
+    /**
+     * Check creator
+     *
+     * @param string $creator
+     */
+    private function checkCreator(&$creator)
+    {
+        if ( ! empty($creator) && ! starts_with($creator, '@')) {
+            $creator = '@' . $creator;
+        }
+    }
+
+    /**
+     * Check description
+     *
+     * @param string $description
+     */
+    private function checkDescription(&$description)
+    {
+        if (empty($description)) {
+            throw new \InvalidArgumentException(
+                'The description attribute must not be empty.'
+            );
+        }
+
+        $description = $this->truncate($description, 200);
+    }
+
+    /**
+     * Check image
+     *
+     * @param string $image
+     */
+    private function checkImage($image)
+    {
+        $this->checkNotEmpty($image, 'The product image is required.');
+    }
+
+    protected function checkNotEmpty($value, $message)
+    {
+        if (empty($value)) {
+            throw new \InvalidArgumentException($message);
+        }
     }
 }
--- tests/Entities/TwitterCard/Cards/ProductTest.php
+++ tests/Entities/TwitterCard/Cards/ProductTest.php
@@ -43,5 +43,224 @@ class ProductTest extends TestCase
     {
         $this->assertInstanceOf(Product::class, $this->card);
         $this->assertEmpty($this->card->render());
+        $this->assertEquals('product', $this->card->getType());
+    }
+
+    /** @test */
+    public function it_can_set_and_get_site()
+    {
+        $username = '@Arcanedev';
+        $this->card->setSite($username);
+
+        $this->assertEquals($username, $this->card->getSite());
+    }
+
+    /** @test */
+    public function it_can_set_and_get_creator()
+    {
+        $creator = 'Arcanedev';
+        $this->card->setCreator($creator);
+
+        $this->assertEquals('@' . $creator, $this->card->getCreator());
+    }
+
+    /** @test */
+    public function it_can_set_and_get_site_by_adding_the_at_symbol()
+    {
+        $username = 'Github';
+        $this->card->setSite($username);
+
+        $this->assertEquals('@' . $username, $this->card->getSite());
+    }
+
+    /**
+     * @test
+     *
+     * @expectedException        \InvalidArgumentException
+     * @expectedExceptionMessage The site attribute must not be empty.
+     */
+    public function it_must_throw_invalid_argument_exception_on_empty_site()
+    {
+        $this->card->setSite('');
+    }
+
+    /** @test */
+    public function it_can_set_and_get_title()
+    {
+        $title = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.';
+        $this->card->setTitle($title);
+
+        $this->assertEquals($title, $this->card->getTitle());
+
+        $title = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+        $this->card->setTitle($title);
+        $title = str_limit($title, 70 - strlen('...'), '...');
+
+        $this->assertEquals($title, $this->card->getTitle());
+    }
+
+    /**
+     * @test
+     *
+     * @expectedException        \InvalidArgumentException
+     * @expectedExceptionMessage The title attribute must not be empty.
+     */
+    public function it_must_throw_invalid_argument_exception_on_empty_title()
+    {
+        $this->card->setTitle('');
+    }
+
+    /** @test */
+    public function it_can_set_and_get_description()
+    {
+        $description = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.';
+        $this->card->setDescription($description);
+
+        $this->assertEquals($description, $this->card->getDescription());
+
+        $description = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+        $this->card->setDescription($description);
+        $description = str_limit($description, 200 - strlen('...'), '...');
+
+        $this->assertEquals($description, $this->card->getDescription());
+    }
+
+    /**
+     * @test
+     *
+     * @expectedException        \InvalidArgumentException
+     * @expectedExceptionMessage The description attribute must not be empty.
+     */
+    public function it_must_throw_invalid_argument_exception_on_empty_description()
+    {
+        $this->card->setDescription('');
+    }
+
+    /** @test */
+    public function it_can_set_and_get_image()
+    {
+        $image = 'http://www.arcanedev.net/uploads/images/posts/thumbs/default/welcome-to-arcanedev.jpg';
+
+        $this->card->setImage($image);
+
+        $this->assertEquals($image, $this->card->getImage());
+    }
+
+    /** @test */
+    public function it_can_set_and_get_data1()
+    {
+        $data1 = 'Deep House';
+        $this->card->setData1($data1);
+
+        $this->assertEquals($data1, $this->card->getData1());
+    }
+
+    /**
+     * @test
+     *
+     * @expectedException        \InvalidArgumentException
+     * @expectedExceptionMessage The product's data1 is required.
+     */
+    public function it_must_throw_invalid_argument_on_empty_data1()
+    {
+        $this->card->setData1('');
+    }
+
+    /** @test */
+    public function it_can_set_and_get_label1()
+    {
+        $label1 = 'C2C';
+        $this->card->setLabel1($label1);
+
+        $this->assertEquals($label1, $this->card->getLabel1());
+    }
+
+    /**
+     * @test
+     *
+     * @expectedException        \InvalidArgumentException
+     * @expectedExceptionMessage The product's label1 is required.
+     */
+    public function it_must_throw_invalid_argument_on_empty_label1()
+    {
+        $this->card->setLabel1('');
+    }
+
+    /** @test */
+    public function it_can_set_and_get_data2()
+    {
+        $data2 = 'Indie';
+        $this->card->setData2($data2);
+
+        $this->assertEquals($data2, $this->card->getData2());
+    }
+
+    /**
+     * @test
+     *
+     * @expectedException        \InvalidArgumentException
+     * @expectedExceptionMessage The product's data2 is required.
+     */
+    public function it_must_throw_invalid_argument_on_empty_data2()
+    {
+        $this->card->setData2('');
+    }
+
+    /** @test */
+    public function it_can_set_and_get_label2()
+    {
+        $label2 = 'Alt-J';
+        $this->card->setLabel2($label2);
+
+        $this->assertEquals($label2, $this->card->getLabel2());
+    }
+
+    /**
+     * @test
+     *
+     * @expectedException        \InvalidArgumentException
+     * @expectedExceptionMessage The product's label2 is required.
+     */
+    public function it_must_throw_invalid_argument_on_empty_label2()
+    {
+        $this->card->setLabel2('');
+    }
+
+    /** @test */
+    public function it_can_render()
+    {
+        $username    = 'Arcanedev';
+        $title       = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.';
+        $description = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.';
+        $image       = 'http://www.arcanedev.net/uploads/images/posts/thumbs/default/welcome-to-arcanedev.jpg';
+        $data1       = 'Deep House';
+        $label1      = 'C2C';
+        $data2       = 'Indie';
+        $label2      = 'Alt-J';
+
+        $this->card->setSite($username);
+        $this->card->setCreator($username);
+        $this->card->setTitle($title);
+        $this->card->setDescription($description);
+        $this->card->setImage($image);
+        $this->card->setData1($data1);
+        $this->card->setLabel1($label1);
+        $this->card->setData2($data2);
+        $this->card->setLabel2($label2);
+
+        $tags = $this->generateTags([
+            'card'        => 'product',
+            'site'        => '@' . $username,
+            'creator'     => '@' . $username,
+            'title'       => $title,
+            'description' => $description,
+            'image'       => $image,
+            'data1'       => $data1,
+            'label1'      => $label1,
+            'data2'       => $data2,
+            'label2'      => $label2,
+        ]);
+
+        $this->assertEquals($tags, $this->card->render());
     }
 }

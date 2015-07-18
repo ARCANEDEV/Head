4bccb57879902f4e90d7f4eb894f196e48dd0c7b
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-07-18 00:46:27 +0000

    Complete the Gallery implementation

--- src/Entities/TwitterCard/Cards/Gallery.php
+++ src/Entities/TwitterCard/Cards/Gallery.php
@@ -1,5 +1,7 @@
 <?php namespace Arcanedev\Head\Entities\TwitterCard\Cards;
 
+use Arcanedev\Head\Entities\TwitterCard\TwitterMetaBuilder as Builder;
+
 /**
  * Class Gallery
  * @package Arcanedev\Head\Entities\TwitterCard\Cards
@@ -12,8 +14,276 @@ class Gallery extends AbstractCard
      |  Properties
      | ------------------------------------------------------------------------------------------------
      */
-    /** @var string */
+    /**
+     * Twitter card type
+     *
+     * @var string
+     */
     protected $card = 'gallery';
+
+    /**
+     * The Twitter @username the card should be attributed to.
+     *
+     * @var string
+     */
+    protected $site        = '';
+
+    /**
+     * @username of content creator
+     *
+     * @var string
+     */
+    protected $creator = '';
+
+    /**
+     * Title should be concise and will be truncated at 70 characters.
+     *
+     * @var string
+     */
+    protected $title       = '';
+
+    /**
+     * A description that concisely summarizes the content of the page,
+     * as appropriate for presentation within a Tweet.
+     * Do not re-use the title text as the description, or use this field
+     * to describe the general services provided by the website.
+     * Description text will be truncated at the word to 200 characters.
+     *
+     * @var string
+     */
+    protected $description = '';
+
+    /**
+     * @var string
+     */
+    protected $url = '';
+
+    /**
+     * @var string
+     */
+    protected $image0 = '';
+
+    /**
+     * @var string
+     */
+    protected $image1 = '';
+
+    /**
+     * @var string
+     */
+    protected $image2 = '';
+
+    /**
+     * @var string
+     */
+    protected $image3 = '';
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Getters & Setters
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Get creator username
+     *
+     * @return string
+     */
+    public function getCreator()
+    {
+        return $this->creator;
+    }
+
+    /**
+     * Set creator username
+     *
+     * @param  string $creator
+     *
+     * @return self
+     */
+    public function setCreator($creator)
+    {
+        $this->checkCreator($creator);
+        $this->creator = $creator;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set description
+     *
+     * @param  string $description
+     *
+     * @return self
+     */
+    public function setDescription($description)
+    {
+        $this->checkDescription($description);
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get Url
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Set Url
+     *
+     * @param  string $url
+     *
+     * @return self
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Get Image 0
+     *
+     * @return string
+     */
+    public function getImage0()
+    {
+        return $this->image0;
+    }
+
+    /**
+     * Set Image 0
+     *
+     * @param string $image0
+     *
+     * @return self
+     */
+    public function setImage0($image0)
+    {
+        $this->image0 = $image0;
+
+        return $this;
+    }
+
+    /**
+     * Get Image 1
+     *
+     * @return string
+     */
+    public function getImage1()
+    {
+        return $this->image1;
+    }
+
+    /**
+     * Set Image 1
+     *
+     * @param  string $image1
+     *
+     * @return self
+     */
+    public function setImage1($image1)
+    {
+        $this->image1 = $image1;
+
+        return $this;
+    }
+
+    /**
+     * Get Image 2
+     *
+     * @return string
+     */
+    public function getImage2()
+    {
+        return $this->image2;
+    }
+
+    /**
+     * Set Image 2
+     *
+     * @param  string $image2
+     *
+     * @return self
+     */
+    public function setImage2($image2)
+    {
+        $this->image2 = $image2;
+
+        return $this;
+    }
+
+    /**
+     * Get Image 3
+     *
+     * @return string
+     */
+    public function getImage3()
+    {
+        return $this->image3;
+    }
+
+    /**
+     * Set Image 3
+     *
+     * @param  string $image3
+     *
+     * @return self
+     */
+    public function setImage3($image3)
+    {
+        $this->image3 = $image3;
+
+        return $this;
+    }
+
+    /**
+     * Get images
+     *
+     * @return array
+     */
+    public function getImages()
+    {
+        return array_filter([
+            $this->getImage0(),
+            $this->getImage1(),
+            $this->getImage2(),
+            $this->getImage3(),
+        ]);
+    }
+
+    /**
+     * Set multiple images - 4 images max
+     *
+     * @param  array $images
+     *
+     * @return self
+     */
+    public function setImages(array $images)
+    {
+        $images = array_slice($images, 0, 4);
+
+        foreach($images as $key => $image) {
+            $this->{'setImage' . $key}($image);
+        }
+
+        return $this;
+    }
 
     /* ------------------------------------------------------------------------------------------------
      |  Main Functions
@@ -30,6 +300,45 @@ class Gallery extends AbstractCard
             return '';
         }
 
-        return '';
+        $attributes = array_intersect_key(get_object_vars($this), array_flip([
+            'card',
+            'site',
+            'creator',
+            'title',
+            'description',
+            'url',
+            'image0',
+            'image1',
+            'image2',
+            'image3',
+        ]));
+
+        return Builder::html($attributes);
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Check Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Check creator
+     *
+     * @param string $creator
+     */
+    private function checkCreator(&$creator)
+    {
+        if ( ! empty($creator) && ! starts_with($creator, '@')) {
+            $creator = '@' . $creator;
+        }
+    }
+
+    /**
+     * Check description
+     *
+     * @param string $description
+     */
+    private function checkDescription(&$description)
+    {
+        $description = $this->truncate($description, 200);
     }
 }
--- tests/Entities/TwitterCard/Cards/GalleryTest.php
+++ tests/Entities/TwitterCard/Cards/GalleryTest.php
@@ -43,5 +43,150 @@ class GalleryTest extends TestCase
     {
         $this->assertInstanceOf(Gallery::class, $this->card);
         $this->assertEmpty($this->card->render());
+        $this->assertEquals('gallery', $this->card->getType());
+    }
+
+    /** @test */
+    public function it_can_set_and_get_creator()
+    {
+        $creator = 'Arcanedev';
+
+        $this->card->setCreator($creator);
+
+        $this->assertEquals('@' . $creator, $this->card->getCreator());
+    }
+
+    /** @test */
+    public function it_can_set_and_get_description()
+    {
+        $description = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.';
+        $this->card->setDescription($description);
+
+        $this->assertEquals($description, $this->card->getDescription());
+
+        $description = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+        $this->card->setDescription($description);
+        $description = str_limit($description, 200 - strlen('...'), '...');
+
+        $this->assertEquals($description, $this->card->getDescription());
+    }
+
+    /** @test */
+    public function it_can_set_and_get_url()
+    {
+        $url = 'https://www.facebook.com/arcanedev.agence.web.casablanca/photos_stream';
+        $this->card->setUrl($url);
+
+        $this->assertEquals($url, $this->card->getUrl());
+    }
+
+    /** @test */
+    public function it_can_set_and_get_image_0()
+    {
+        $image = 'http://www.arcanedev.net/uploads/images/posts/thumbs/default/welcome-to-arcanedev.jpg';
+        $this->card->setImage0($image);
+
+        $this->assertEquals($image, $this->card->getImage0());
+    }
+
+    /** @test */
+    public function it_can_set_and_get_image_1()
+    {
+        $image = 'http://www.arcanedev.net/uploads/images/posts/thumbs/default/welcome-to-arcanedev.jpg';
+        $this->card->setImage1($image);
+
+        $this->assertEquals($image, $this->card->getImage1());
+    }
+
+    /** @test */
+    public function it_can_set_and_get_image_2()
+    {
+        $image = 'http://www.arcanedev.net/uploads/images/posts/thumbs/default/welcome-to-arcanedev.jpg';
+        $this->card->setImage2($image);
+
+        $this->assertEquals($image, $this->card->getImage2());
+    }
+
+    /** @test */
+    public function it_can_set_and_get_image_3()
+    {
+        $image = 'http://www.arcanedev.net/uploads/images/posts/thumbs/default/welcome-to-arcanedev.jpg';
+        $this->card->setImage3($image);
+
+        $this->assertEquals($image, $this->card->getImage3());
+    }
+
+    /** @test */
+    public function it_can_set_and_get_multiple_images()
+    {
+        $images = [
+            'http://www.arcanedev.net/uploads/images/posts/thumbs/default/welcome-to-arcanedev.jpg',
+            'http://www.arcanedev.net/assets/img/arcanedev-responsive.png',
+            'http://www.arcanedev.net/assets/img/slider/5/mac.png',
+            'http://www.arcanedev.net/assets/img/slider/5/macbook.png',
+        ];
+
+        $this->card->setImages($images);
+
+        $this->assertCount(4, $this->card->getImages());
+        $this->assertEquals($images, $this->card->getImages());
+    }
+
+    /** @test */
+    public function it_can_set_and_get_multiple_images_with_limit()
+    {
+        $images = [
+            'http://www.arcanedev.net/uploads/images/posts/thumbs/default/welcome-to-arcanedev.jpg',
+            'http://www.arcanedev.net/assets/img/arcanedev-responsive.png',
+            'http://www.arcanedev.net/assets/img/slider/5/mac.png',
+            'http://www.arcanedev.net/assets/img/slider/5/macbook.png',
+
+            'http://www.arcanedev.net/uploads/images/posts/thumbs/default/welcome-to-arcanedev.jpg',
+            'http://www.arcanedev.net/assets/img/arcanedev-responsive.png',
+            'http://www.arcanedev.net/assets/img/slider/5/mac.png',
+            'http://www.arcanedev.net/assets/img/slider/5/macbook.png',
+        ];
+
+        $this->card->setImages($images);
+
+        $this->assertCount(4, $this->card->getImages());
+        $this->assertEquals(array_slice($images, 0, 4), $this->card->getImages());
+    }
+
+    /** @test */
+    public function it_can_render()
+    {
+        $username    = 'Arcanedev';
+        $title       = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.';
+        $description = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.';
+        $url         = 'https://www.facebook.com/arcanedev.agence.web.casablanca/photos_stream';
+        $images      = [
+            'http://www.arcanedev.net/uploads/images/posts/thumbs/default/welcome-to-arcanedev.jpg',
+            'http://www.arcanedev.net/assets/img/arcanedev-responsive.png',
+            'http://www.arcanedev.net/assets/img/slider/5/mac.png',
+            'http://www.arcanedev.net/assets/img/slider/5/macbook.png',
+        ];
+
+        $this->card->setSite($username);
+        $this->card->setCreator($username);
+        $this->card->setTitle($title);
+        $this->card->setDescription($description);
+        $this->card->setUrl($url);
+        $this->card->setImages($images);
+
+        $tags = $this->generateTags([
+            'card'        => 'gallery',
+            'site'        => '@' . $username,
+            'creator'     => '@' . $username,
+            'title'       => $title,
+            'description' => $description,
+            'url'         => $url,
+            'image0'      => $images[0],
+            'image1'      => $images[1],
+            'image2'      => $images[2],
+            'image3'      => $images[3],
+        ]);
+
+        $this->assertEquals($tags, $this->card->render());
     }
 }
